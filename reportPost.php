797199884bd676e7e6d53f0ab52bedf9a502af88
Jonathon <jonathonc.94@gmail.com>
2018-01-31 16:20:48 -0500

    Finally it is workinggit add reportPost.php

--- reportPost.php
+++ reportPost.php
@@ -1,4 +1,9 @@
 <?php
+
+//Debugging Code Snippet
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
 $winNumber = $_POST["win_number"];
 $dateOfIncident = $_POST["date_of_incident"];
 $imageFile = $_FILES["image"];
@@ -9,15 +14,20 @@ $username = "plugs";
 $password = "phpplugs";
 $dbName = "phpplugs";
 
-$imageTargetDir = "/";
-$imageTargetFile = $imageTargetDir . $winNumber . "_" . time();
+$imageFileType = strtolower(pathinfo($imageFile["name"],PATHINFO_EXTENSION));
+$imageTargetDir = "/var/www/html/phpplugs/m/";
+$imageTargetFile = $imageTargetDir . $winNumber . "_" . time() . $imageFileType;
 
 if (move_uploaded_file($imageFile['tmp_name'], $imageTargetFile)) {
     echo "File is valid, and was uploaded\n";
+
 }
 else {
     echo "Possible problem with file. \n";
-}
+    echo "<br>";
+    echo "Unable to store file: " . $imageTargetFile;
+    echo "<br><br>";
+    }
 
 $conn = new mysqli($servername, $username, $password, $dbName);
 
